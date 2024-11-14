15cd6e1f44cfeb5ecc4089695c50ba22e85171c5
zhf <1440837150@qq.com>
2024-11-14 16:09:07 +0800

    测试添加东西

--- cmd/mycmd.go
+++ cmd/mycmd.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"gitlab.zhf.cn/myhpa_controller/pkg/apis/mygroup.example.com/v1alpha1"
 	"gitlab.zhf.cn/myhpa_controller/pkg/clientset/clientset"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/tools/clientcmd"
@@ -30,5 +31,20 @@ func main() {
 	}
 	for _, res := range list.Items {
 		fmt.Printf("%s\n", res.GetName())
+	}
+	var myresource v1alpha1.MyResource = v1alpha1.MyResource{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "sss1",
+			Namespace: "default",
+		},
+		Spec: v1alpha1.MyResourceSpec{
+			Image: "sss",
+		},
+	}
+	_, err = clientset.MygroupV1alpha1().MyResources("default").Create(context.TODO(),
+		&myresource, metav1.CreateOptions{})
+	if err != nil {
+		println("ssad")
+		panic(err)
 	}
 }
